739e13bacfa9ac7ac5ebfc144835c9b1f3364cb4
woojin <kin710730@kumoh.ac.kr>
2026-09-21 17:39:01 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={ () => {
+        setCount(prev => prev + 1)
+      } }>
+        증가
+      </button>
+    </>
+  )
+}
+
+export default App
